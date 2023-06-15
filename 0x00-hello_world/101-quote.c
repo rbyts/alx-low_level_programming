f0bbf2b797798a47449896fbe170d47153b63159
rbyts <speqcs@gmail.com>
2023-06-15 15:53:46 -0700

    update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - The main method of the program
+ *
+ * This function print a message with printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+  return (1);
+}
