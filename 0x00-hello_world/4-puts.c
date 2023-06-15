4a1051443cb91e6484051e00b63af79d86571c17
rbyts <speqcs@gmail.com>
2023-06-15 15:38:51 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
-/**
- * 
+/** 
  *  main - Entry point of the program 
  *  
  *  This function prints a string
  *  using the puts function and returns 0 to indicate successful execution.
  *  
  *  Return: Always 0 (Success)
- *
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
