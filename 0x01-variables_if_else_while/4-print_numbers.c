232002ad9170c25f3a44012ce63033b675c95907
rbyts <speqcs@gmail.com>
2023-06-16 16:44:25 -0700

    update

---
+++ 0x01-variables_if_else_while/4-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - The main method of the program
+ *
+ * This function print alphabet from a to z
+ * without printing q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
