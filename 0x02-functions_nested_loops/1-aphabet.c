7b467bcfdd99f365a02bb5e637fc93fc1162a17e
rbyts <speqcs@gmail.com>
2023-06-20 15:05:13 -0700

    update

---
+++ 0x02-functions_nested_loops/1-aphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - as the name of the function
+*
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+char i;
+for (i = 'a'; i != '{'; i++)
+{
+_putchar(i);
+};
+_putchar('\n');
+}
