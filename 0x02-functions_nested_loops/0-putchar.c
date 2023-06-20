b14efc31ff1453a87625770695d57906c23e1515
rbyts <speqcs@gmail.com>
2023-06-20 14:57:10 -0700

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* main - here we describe the function but im too busy for that.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+char s[] = "_putchar\n";
+int i;
+for (i = 0; i < 9; i++)
+{
+_putchar(s[i]);
+}
+return (0);
+}
