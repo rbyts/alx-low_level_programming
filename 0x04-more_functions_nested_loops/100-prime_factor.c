cb738b4f181460b895b6d5bbf1e7f487a987d9cd
rbyts <speqcs@gmail.com>
2023-06-22 15:29:24 -0700

    update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -15,25 +15,25 @@ int main(void)
   n = 612852475143;
   max = -1;
 
-  while (n % 2 == 0)
+while (n % 2 == 0)
     {
       max = 2;
       n /= 2;
     }
 
-  for (i = 3; i <= sqrt(n); i = i + 2)
+for (i = 3; i <= sqrt(n); i = i + 2)
     {
-      while (n % i == 0)
+while (n % i == 0)
 	{
 	  max = i;
 	  n = n / i;
 	}
     }
 
-  if (n > 2)
+if (n > 2)
     max = n;
 
   printf("%ld\n", max);
 
-  return (0);
+return (0);
 }
