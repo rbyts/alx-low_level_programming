2b0e98e64af1711f6d8983c75653ec70def30eb9
rbyts <speqcs@gmail.com>
2023-06-26 14:50:21 -0700

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - as the name of the function
+ *
+ * @n: parameter of the function
+ *
+ * Return: depending on the function.
+ */
+void reset_to_98(int *n)
+{
+  *n = 98;
+}
