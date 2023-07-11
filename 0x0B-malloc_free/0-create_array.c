99e442f0dbfffab60aab0971a1f8974a5b5aad90
rbyts <speqcs@gmail.com>
2023-07-11 13:18:53 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,8 +5,7 @@
  * @size: size of array
  * @c: char to assign
  * Description: creat array of size size and assign char c
- * Return: pointer to array, NULL if fail
- * 
+ * Return: pointer to array, NULL if fail 
 */
 char *create_array(unsigned int size, char c)
 {
